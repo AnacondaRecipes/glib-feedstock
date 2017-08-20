c5a21561d7c34e2acb96e91de0f37f3dc3db04b6
Ray Donnelly <mingw.android@gmail.com>
2017-08-20 14:27:28 +0100

    Add cross-compilation support and do not depend on libiconv on Linux
    .. because it is built in to glibc.
    
    Also make verbosely.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+<<<<<<< HEAD
 if [[ $(uname) == Darwin ]]; then
   export CC=clang
   export CXX=clang++
@@ -35,6 +36,38 @@ make -j$CPU_COUNT
 make install -j$CPU_COUNT
 
 export PYTHON=$_PY
+=======
+# Need to get appropriate response to g_get_system_data_dirs()
+# See the hardcoded-paths.patch file
+export CFLAGS="-DCONDA_PREFIX=\\\"${PREFIX}\\\""
+
+if [[ ${HOST} =~ .*darwin.* ]]; then
+  # Pick up the Conda version of gettext/libintl:
+  export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+  export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib -Wl,-rpath,${PREFIX}/lib"
+  LIBICONV=gnu
+elif [[ ${HOST} =~ .*linux.* ]]; then
+  # Pick up the Conda version of gettext/libintl:
+  export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+  export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+  # So the system (builtin to glibc) iconv gets found and used.
+  LIBICONV=maybe
+fi
+
+# A full path to PYTHON causes overly long shebang in gobject/glib-genmarshal
+./configure --prefix=${PREFIX} \
+            --with-python=$(basename "${PYTHON}") \
+            --with-libiconv=${LIBICONV} \
+            --disable-libmount \
+                || { cat config.log; exit 1; }
+
+make -j${CPU_COUNT} V=1
+# FIXME
+# ERROR: fileutils - too few tests run (expected 15, got 14)
+# ERROR: fileutils - exited with status 134 (terminated by signal 6?)
+# make check
+make install
+>>>>>>> Add cross-compilation support and do not depend on libiconv on Linux
 
 cd $PREFIX
 find . -type f -name "*.la" -exec rm -rf '{}' \; -print
