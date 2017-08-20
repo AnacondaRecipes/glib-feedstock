81e0151a62b2ebeddd6967c51dcd5b5600f9327b
Ray Donnelly <mingw.android@gmail.com>
2017-08-20 14:27:28 +0100

    Add cross-compilation support and do not depend on libiconv on Linux
    .. because it is built in to glibc.
    
    Also make verbosely.

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,30 +4,27 @@
 # See the hardcoded-paths.patch file
 export CFLAGS="-DCONDA_PREFIX=\\\"${PREFIX}\\\""
 
-if [ "$(uname)" == "Darwin" ] ; then
-  export CC=clang
-  export CXX=clang++
-  # Cf. the discussion in meta.yaml -- we require 10.7.
-  export MACOSX_DEPLOYMENT_TARGET="10.7"
-  SDK=/
-  export CFLAGS="${CFLAGS} -isysroot ${SDK}"
-  export LDFLAGS="${LDFLAGS} -Wl,-syslibroot,${SDK}"
+if [[ ${HOST} =~ .*darwin.* ]]; then
   # Pick up the Conda version of gettext/libintl:
   export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
   export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib -Wl,-rpath,${PREFIX}/lib"
-elif [ "$(uname)" == "Linux" ] ; then
+  LIBICONV=gnu
+elif [[ ${HOST} =~ .*linux.* ]]; then
   # Pick up the Conda version of gettext/libintl:
   export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
   export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+  # So the system (builtin to glibc) iconv gets found and used.
+  LIBICONV=maybe
 fi
 
+# A full path to PYTHON causes overly long shebang in gobject/glib-genmarshal
 ./configure --prefix=${PREFIX} \
-            --with-python="${PYTHON}" \
-            --with-libiconv=gnu \
+            --with-python=$(basename "${PYTHON}") \
+            --with-libiconv=${LIBICONV} \
             --disable-libmount \
                 || { cat config.log; exit 1; }
 
-make
+make -j${CPU_COUNT} V=1
 # FIXME
 # ERROR: fileutils - too few tests run (expected 15, got 14)
 # ERROR: fileutils - exited with status 134 (terminated by signal 6?)
