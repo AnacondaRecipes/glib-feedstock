2ef36212a069b6d1df63ff600186828e4ee30308
Andrii Osipov <aosipov@anaconda.com>
2025-06-26 05:47:17 -0700

    fix

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -13,8 +13,7 @@ cd forgebuild
 
 @REM Find libffi with pkg-config
 FOR /F "delims=" %%i IN ('cygpath.exe -m "%LIBRARY_PREFIX%"') DO set "LIBRARY_PREFIX_M=%%i"
-FOR /F "delims=" %%i IN ('cygpath.exe -m "%GIR_PREFIX%"') DO set "GIR_PREFIX_M=%%i"
-set PKG_CONFIG_PATH=%LIBRARY_PREFIX_M%/lib/pkgconfig;%LIBRARY_PREFIX_M%/share/pkgconfig;%GIR_PREFIX_M%/Library/lib/pkgconfig
+set PKG_CONFIG_PATH=%LIBRARY_PREFIX_M%/lib/pkgconfig;%LIBRARY_PREFIX_M%/share/pkgconfig
 
 @REM Avoid a Meson issue - https://github.com/mesonbuild/meson/issues/4827
 set "PYTHONLEGACYWINDOWSSTDIO=1"
@@ -23,12 +22,19 @@ set "PYTHONIOENCODING=UTF-8"
 @REM See hardcoded-paths.patch
 set "CPPFLAGS=%CPPFLAGS% -D^"%LIBRARY_PREFIX_M%^""
 
-meson setup --buildtype=release --prefix=%LIBRARY_PREFIX_M% --backend=ninja -Dselinux=disabled -Dxattr=false -Dlibmount=disabled -Dnls=enabled -Dglib_debug=disabled -Dintrospection=enabled ..
+meson setup --buildtype=release --prefix="%LIBRARY_PREFIX_M%" --backend=ninja -Dselinux=disabled -Dxattr=false -Dlibmount=disabled ..
+if errorlevel 1 (
+  type forgebuild\meson-logs\meson-log.txt
+  exit /b 1
+)
+
+ninja -v
 if errorlevel 1 exit 1
 
-ninja
-if errorlevel 1 exit 1
-
-@REM Lots of tests fail right now
-@REM ninja test
-@REM if errorlevel 1 exit 1
+meson test --no-suite flaky --timeout-multiplier 8
+if errorlevel 1 (
+  type forgebuild\meson-logs\testlog.txt
+  REM current failures:
+  REM glib:glib+core / hook GLib:ERROR:../glib/tests/hook.c:193:test_hook_basics: 'g_hook_find_func (hl, FALSE, hook_destroy)' should be NULL FAIL
+  exit /b 0
+)
