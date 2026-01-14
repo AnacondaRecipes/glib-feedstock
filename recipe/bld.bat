db68cb998f0d94d133e8a34a15c61f82dda5bf76
vchychy <vchychyrko@anaconda.com>
2026-01-14 15:57:31 +0200

    add: win g-ir-build-tools to build

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -8,11 +8,20 @@ mkdir %BUILD_PREFIX%\Library\etc
 echo none / cygdrive binary,user 0 0 >%BUILD_PREFIX%\Library\etc\fstab
 echo none /tmp usertemp binary,posix=0 0 0 >>%BUILD_PREFIX%\Library\etc\fstab
 
+set "GIR_PREFIX=%cd%\g-ir-prefix"
 
+call conda create -p %GIR_PREFIX% -y -c conda-forge -c defaults g-ir-build-tools gobject-introspection glib "setuptools<71"
+if errorlevel 1 exit 1
+
+set "PATH=%PATH%;%GIR_PREFIX%\Library;%GIR_PREFIX%\Library\bin"
+
+mkdir forgebuild
+cd forgebuild
 
 @REM Find libffi with pkg-config
 FOR /F "delims=" %%i IN ('cygpath.exe -m "%LIBRARY_PREFIX%"') DO set "LIBRARY_PREFIX_M=%%i"
-set PKG_CONFIG_PATH=%LIBRARY_PREFIX_M%/lib/pkgconfig;%LIBRARY_PREFIX_M%/share/pkgconfig
+FOR /F "delims=" %%i IN ('cygpath.exe -m "%GIR_PREFIX%"') DO set "GIR_PREFIX_M=%%i"
+set PKG_CONFIG_PATH=%LIBRARY_PREFIX_M%/lib/pkgconfig;%LIBRARY_PREFIX_M%/share/pkgconfig;%GIR_PREFIX_M%/Library/lib/pkgconfig
 
 @REM Avoid a Meson issue - https://github.com/mesonbuild/meson/issues/4827
 set "PYTHONLEGACYWINDOWSSTDIO=1"
@@ -20,9 +29,6 @@ set "PYTHONIOENCODING=UTF-8"
 
 @REM See hardcoded-paths.patch
 set "CPPFLAGS=%CPPFLAGS% -D^"%LIBRARY_PREFIX_M%^""
-
-mkdir forgebuild
-cd forgebuild
 
 meson setup --buildtype=release --prefix="%LIBRARY_PREFIX_M%" --backend=ninja -Dselinux=disabled -Dxattr=false -Dlibmount=disabled -Dintrospection=enabled ..
 if errorlevel 1 (
